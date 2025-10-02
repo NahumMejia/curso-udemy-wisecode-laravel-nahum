2c37487ba0471cc0be4958ff7f65b08e84922e49
Nahum <nahummejiad@gmail.com>
2025-10-01 19:41:00 -0600

    Correción en migración

--- database/migrations/2025_10_02_012422_product_warehouses.php
+++ database/migrations/2025_10_02_012422_product_warehouses.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('product_wallets', function (Blueprint $table) { 
+        Schema::create('product_warehouses', function (Blueprint $table) { 
         $table->id();
         $table->bigInteger('product_id');
         $table->bigInteger('unit_id');
