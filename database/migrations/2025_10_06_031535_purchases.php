f8f7b5f355346edd9ddfa6248076bb4ec8320c38
Nahum <nahummejiad@gmail.com>
2025-10-05 21:23:12 -0600

    Añadidas migraciones de detalles de compras

---
+++ database/migrations/2025_10_06_031535_purchases.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('purchases', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->timestamp('date_emision')->nullable();
+            $table->timestamp('date_entrega')->nullable();
+            $table->softDeletes();
+            $table->string('type_comprobant'); 
+            $table->double('total');
+            $table->double('importe');
+            $table->double('igv');
+            $table->string('n_comprobant')->nullable();; 
+            $table->string('description')->nullable();; 
+            $table->bigInteger('warehouse_id')->nullable();;
+            $table->bigInteger('user_id')->nullable();;
+            $table->bigInteger('provider_id')->nullable();;
+            $table->tinyInteger('state')->default(1);
+    });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
---
+++ database/migrations/2025_10_06_031923_purchase_details.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('purchase_details', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->timestamp('date_entrega')->nullable();
+            $table->softDeletes();
+            $table->string('description')->nullable();
+            $table->double('quantity')->nullable();
+            $table->double('price_unit')->nullable();
+            $table->double('total')->nullable();
+            $table->bigInteger('purchase_id');
+            $table->bigInteger('product_id');
+            $table->bigInteger('unit_id');
+            $table->bigInteger('user_entrega');
+            $table->tinyInteger('state')->default(1);
+    });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
